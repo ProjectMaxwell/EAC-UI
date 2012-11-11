1b47a49d0b26526b304235a7ca06bac1548edf35
Kevin <github.jade@McGuireKevin.com>
2012-11-10 21:08:47 -0800

    Everything I have ever done

---
+++ Authenticate.js
@@ -0,0 +1,75 @@
+var authResponse;
+function authenticateByPassword(username, password){
+	var alertString = "";
+	if(username && password){
+		var allData = {
+				"grantType": "PASSWORD",
+				"username": username,
+				"password": password,
+				"clientId": "MAXWELL_WEB_CLIENT"
+			};
+		allData = JSON.stringify(allData);
+		jQuery.ajax({
+			type: "POST",
+			data: allData,
+			url: "http://evergreenalumniclub.com:7080/PhiAuth/rest/token",
+			dataType: "JSON",
+			contentType: "application/json",
+			success: function(response){
+				authResponse = response;
+				alertString = response;
+				return response;
+			},
+			error: function(){
+				alertString = JSON.stringify(response.responseText);
+			},
+			complete: function(){
+				alert(alertString);
+			}
+		})
+	}else{
+		if(!username){
+			alertString = "Enter a username. ";
+		}
+		if(!password){
+			if(alertString != ""){
+				alertString += " Also, you";
+			}else{
+				alertString = "You";
+			}
+			alertString += " are missing a password";
+		}
+		alert(alertString);
+	}
+}
+function authenticateByRefresh(refreshToken){
+	var alertString = "";
+	if(refreshToken){
+		var allData = {
+			"grantType": "REFRESH",
+			"refreshToken": refreshToken
+			};
+		allData = JSON.stringify(allData);
+		jQuery.ajax({
+			type: "POST",
+			data: allData,
+			url: "http://evergreenalumniclub.com:7080/PhiAuth/rest/token",
+			dataType: "JSON",
+			contentType: "application/json",
+			success: function(response){
+				alertString = response;
+			},
+			error: function(response){
+				alertString = JSON.stringify(response.responseText);
+			},
+			complete: function(){
+				alert(alertString);
+			}
+		})
+	}else{
+		alert("You have not yet logged in");
+	}
+}
+function authenticateByClient(){
+	alert("Hooray");
+}
---
+++ ThinClient.js
@@ -0,0 +1,57 @@
+/** Everyone says that we should detect browsers not by detecting browsers but by detecting objects.
+    So I went with the only technology I could think of AJAX, plus we had some code lying around
+    It detects if the browser is doing AJAX the IE way or the other way. and returns if the browser is IE or not.
+ */
+function browserAJAX() {
+  if (window.ActiveXObject){
+    return "IE";
+  }
+  else if (window.XMLHttpRequest){
+    return "Not IE";
+  }
+}
+
+// This is decidedly not how you determine if something is mobile or not, as I learned when I tested it on my phone.
+function screenSize() {
+  if (screen.height <= 800 && screen.width <= 600){
+    var screenSize = "mobile";
+  } else {
+    var screenSize = "normal";
+  }
+}
+
+// This function loads the files of the not-thin Client
+function addFile(filename, filetype){
+//if filename is a external JavaScript file create a script element and add the js file.
+ if (filetype == "js"){ 
+  var element = document.createElement('script');
+  element.setAttribute("type","text/javascript");
+  element.setAttribute("src", filename);
+ }
+ //if filename is an external CSS file create a link element and load a stylesheet.
+ else if (filetype == "css"){ 
+  var element = document.createElement("link");
+  element.setAttribute("rel", "stylesheet");
+  element.setAttribute("type", "text/css");
+  element.setAttribute("href", filename);
+ }
+}
+
+// Loads the not-thin client
+function loadClient(browser, screenSize){
+switch (browser){
+case IE:
+    addFile("IE.js", "js");
+  break;
+default:
+    addFile("firefox.js", "js");
+}
+
+switch (screenSize){
+case mobile
+    addFile("mobile.css", "css");
+    break;
+default:
+    addFile("maxwell.css", "css");
+    }
+}
